56736b043243544460c1adf05311dd238d35b2b4
thaihuuhung636-collab <thaihuuhung636@gmail.com>
2026-06-13 12:35:36 +0700

    Update styles/styles.js

---
+++ styles/styles.js
@@ -0,0 +1,224 @@
+var qgis2webFloodStyles = {
+  "base_Mặt_cắt_khảo_sát": {
+    "type": "single",
+    "style": {
+      "strokeColor": "#000000",
+      "fillColor": "#000000",
+      "strokeWidth": 2.8346,
+      "radius": 5,
+      "iconUrl": "styles/legend/base_Mặt_cắt_khảo_sát_style_001.png",
+      "iconScale": 1.0,
+      "iconSize": [
+        32,
+        32
+      ],
+      "lineCap": "butt",
+      "strokeDash": [
+        15.0,
+        6.0
+      ]
+    }
+  },
+  "base_Vi_Tri_diem_48N": {
+    "type": "categorized",
+    "field": "Layer",
+    "categories": [
+      {
+        "value": "Bệnh viện",
+        "label": "Bệnh viện",
+        "style": {
+          "strokeColor": "#e31a1c",
+          "fillColor": "#e31a1c",
+          "strokeWidth": 0.56692,
+          "radius": 2.95384,
+          "iconUrl": "styles/legend/base_Vi_Tri_diem_48N_style_001.png",
+          "iconScale": 1.0,
+          "iconSize": [
+            32,
+            32
+          ],
+          "color": "#ffffff",
+          "lineCap": "butt"
+        }
+      },
+      {
+        "value": "Trụ sở hành chính",
+        "label": "Trụ sở hành chính",
+        "style": {
+          "strokeColor": "#000000",
+          "fillColor": "#000000",
+          "strokeWidth": 1.13384,
+          "radius": 2,
+          "iconUrl": "styles/legend/base_Vi_Tri_diem_48N_style_002.png",
+          "iconScale": 1.0,
+          "iconSize": [
+            32,
+            32
+          ],
+          "color": "#ffffff",
+          "lineCap": "butt"
+        }
+      }
+    ],
+    "defaultStyle": {
+      "strokeColor": "#232323",
+      "fillColor": "#c43c39",
+      "strokeWidth": 0.35,
+      "radius": 3.2,
+      "iconUrl": "styles/legend/base_Vi_Tri_diem_48N_style_003.png",
+      "iconScale": 1.0,
+      "iconSize": [
+        32,
+        32
+      ],
+      "color": "#c43c39",
+      "lineCap": "butt"
+    }
+  },
+  "base_Duong_giao_Thong_48N": {
+    "type": "categorized",
+    "field": "Layer",
+    "categories": [
+      {
+        "value": "Đường Quốc Lộ",
+        "label": "Đường Quốc Lộ",
+        "style": {
+          "strokeColor": "#000000",
+          "fillColor": "#000000",
+          "strokeWidth": 2.8346,
+          "radius": 5,
+          "iconUrl": "styles/legend/base_Duong_giao_Thong_48N_style_001.png",
+          "iconScale": 1.0,
+          "iconSize": [
+            32,
+            32
+          ],
+          "lineCap": "butt",
+          "strokeDash": [
+            15.0,
+            6.0
+          ]
+        }
+      },
+      {
+        "value": "Đường sắt",
+        "label": "Đường sắt",
+        "style": {
+          "strokeColor": "#000000",
+          "fillColor": "#000000",
+          "strokeWidth": 6.80304,
+          "radius": 5,
+          "iconUrl": "styles/legend/base_Duong_giao_Thong_48N_style_002.png",
+          "iconScale": 1.0,
+          "iconSize": [
+            32,
+            32
+          ],
+          "lineCap": "square",
+          "lineJoin": "miter",
+          "strokeDash": [
+            15.0,
+            6.0
+          ],
+          "lineLayers": [
+            {
+              "strokeColor": "#000000",
+              "fillColor": "#000000",
+              "strokeWidth": 1.13384,
+              "lineCap": "square",
+              "lineJoin": "miter",
+              "strokeDash": [
+                15.0,
+                6.0
+              ]
+            },
+            {
+              "strokeColor": "#000000",
+              "fillColor": "#000000",
+              "strokeWidth": 6.80304
+            }
+          ]
+        }
+      },
+      {
+        "value": "Đường Tỉnh Lộ",
+        "label": "Đường Tỉnh Lộ",
+        "style": {
+          "strokeColor": "#000000",
+          "fillColor": "#000000",
+          "strokeWidth": 1.4173,
+          "radius": 5,
+          "iconUrl": "styles/legend/base_Duong_giao_Thong_48N_style_003.png",
+          "iconScale": 1.0,
+          "iconSize": [
+            32,
+            32
+          ],
+          "lineCap": "butt",
+          "strokeDash": [
+            15.0,
+            6.0
+          ]
+        }
+      },
+      {
+        "value": "None",
+        "label": "None",
+        "style": {
+          "strokeColor": "#000000",
+          "fillColor": "#000000",
+          "strokeWidth": 0.736996,
+          "radius": 5,
+          "iconUrl": "styles/legend/base_Duong_giao_Thong_48N_style_004.png",
+          "iconScale": 1.0,
+          "iconSize": [
+            32,
+            32
+          ],
+          "lineCap": "butt",
+          "strokeDash": [
+            15.0,
+            6.0
+          ]
+        }
+      }
+    ],
+    "defaultStyle": {
+      "strokeColor": "#000000",
+      "fillColor": "#000000",
+      "strokeWidth": 0.736996,
+      "radius": 5,
+      "iconUrl": "styles/legend/base_Duong_giao_Thong_48N_style_005.png",
+      "iconScale": 1.0,
+      "iconSize": [
+        32,
+        32
+      ],
+      "lineCap": "butt",
+      "strokeDash": [
+        15.0,
+        6.0
+      ]
+    }
+  },
+  "base_Ranh_giới_tỉnh": {
+    "type": "single",
+    "style": {
+      "strokeColor": "#000000",
+      "fillColor": "#000000",
+      "strokeWidth": 2.8346,
+      "radius": 5,
+      "iconUrl": "styles/legend/base_Ranh_giới_tỉnh_style_001.png",
+      "iconScale": 1.0,
+      "iconSize": [
+        32,
+        32
+      ],
+      "lineCap": "butt",
+      "strokeDash": [
+        15.0,
+        6.0
+      ]
+    }
+  }
+};
